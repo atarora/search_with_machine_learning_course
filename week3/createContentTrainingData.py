b2fb440e7065e9463578b1e8f2b67ef9d5884ee9
Atita Arora <atitaarora@gmail.com>
2022-03-06 15:17:45 +0000

    Project task 1

--- week3/createContentTrainingData.py
+++ week3/createContentTrainingData.py
@@ -1,12 +1,32 @@
 import argparse
 import os
 import random
+import pandas as pd
 import xml.etree.ElementTree as ET
 from pathlib import Path
+from nltk.stem import SnowballStemmer
 
 def transform_name(product_name):
-    # IMPLEMENT
+    # Transforming the name strings to lowercase, changing punctuation characters  
+    # to spaces, and stemming (you can use the NLTK Snowball stemmer)
+    punctuation= '''!()-[]{};:'"\, <>./?@#$%^&*_~'''
+    emptyString=""
+    spaceString=" "
+    product_name.lower()
+    for x in punctuation:
+        product_name=product_name.replace(x,spaceString)
+    stemmer = SnowballStemmer("english")
+    product_name = " ".join((stemmer.stem(w) for w in product_name.split()))
     return product_name
+
+def count_cats(outputfile,min_products):
+    df = pd.read_csv(outputfile,names=['category'])
+    df[['category','name_of_product']] = df["category"].str.split(" ", 1, expand=True)
+    df["name_of_product"] = df["name_of_product"].str.strip("-")
+    category_group = df['category'].value_counts()[lambda x: x>min_products].index.tolist()
+    print("Writing results to %s" % outputfile+'.withCatLimit.'+str(min_products))
+    (df[df['category'].isin(category_group)]).to_csv(outputfile+'.withCatLimit'+str(min_products),header=False,index=False)
+
 
 # Directory for product data
 directory = r'/workspace/search_with_machine_learning_course/data/pruned_products/'
@@ -35,7 +55,7 @@ if args.input:
 min_products = args.min_products
 sample_rate = args.sample_rate
 
-print("Writing results to %s" % output_file)
+print("Preparing results to %s" % output_file)
 with open(output_file, 'w') as output:
     for filename in os.listdir(directory):
         if filename.endswith(".xml"):
@@ -55,4 +75,4 @@ with open(output_file, 'w') as output:
                       # Replace newline chars with spaces so fastText doesn't complain
                       name = child.find('name').text.replace('\n', ' ')
                       output.write("__label__%s %s\n" % (cat, transform_name(name)))
-
+count_cats(output_file, min_products)                      
